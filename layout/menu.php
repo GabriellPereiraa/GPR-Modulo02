dd51c59981379f8b879aaf4f56f2bc1d375f5dee
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-08-06 09:18:22 -0300

    update

---
+++ layout/menu.php
@@ -0,0 +1,55 @@
+<style>
+    .MenuTopico {
+        color: #274F23 !important;
+        font-weight: bold !important;
+    }
+</style>
+
+<div id="app">
+    <div id="sidebar" class="active">
+        <div class="sidebar-wrapper active ps ps--active-y">
+            <div class="m-4">
+                <!-- <a href="index.php">
+                    <img class="img-fluid" src="https://atilasilvalima.a2hosted.com/projects/EGPCE/Cedis/cdn/imagens/egpce.png" alt="Logo EGPCE">
+                </a> -->
+                <img class="img-fluid" src="https://www.egp.ce.gov.br/wp-content/uploads/sites/8/2021/06/logo-egpce-250x170-1.png" alt="Logo EGPCE">
+            </div>
+            <div class="sidebar-menu">
+                
+                <ul class="menu ms-3">
+                    <li class="sidebar-item">
+                        <a href="../Topico01.php" class="MenuTopico sidebar-link">Tópico 01 - Políticas Públicas: origens e conceitos</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Topico02.php" class="MenuTopico sidebar-link">Tópico 02 - Formulação e Análise de Políticas Públicas</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Topico03.php" class="MenuTopico sidebar-link">Tópico 03 - Ciclo de Políticas Públicas</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Topico04.php" class="MenuTopico sidebar-link">Tópico 04 - Atores Políticos (stakeholders)</a>
+                    </li>
+                    <li class="sidebar-item has-sub">
+                        <a href="#" class="MenuTopico sidebar-link">Tópico 05 - Políticas Públicas no Brasil entre os anos 1994 a 2016</a>
+
+                        <ul class="submenu">
+                            <li class="sidebar-item">
+                                <a href="../Topico05.php#SubTopico5.1" class="sidebar-sub">
+                                    <span>5.1 Políticas Públicas no Governo FHC</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="../Topico05.php#SubTopico5.2" class="sidebar-sub">
+                                    <span>5.2 As políticas públicas nos governos Lula e Dilma</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="../Referencias.php" class="MenuTopico sidebar-link">Referencias</a>
+                    </li>
+                </ul>
+            </div>
+            <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+        </div>
+    </div>
